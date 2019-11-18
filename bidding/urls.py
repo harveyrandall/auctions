904cdbf2807c025a574f87f9fbeaf94c0e777767
Harvey Randall <harvey_randall@me.com>
2019-11-18 01:52:48 +0000

    add new urls, views for the urls, and templates for the views

--- bidding/urls.py
+++ bidding/urls.py
@@ -18,8 +18,11 @@ urlpatterns = [
         name="logout"),
     path('register/', views.RegisterView.as_view(), name="register"),
     path('search/', views.SearchListView.as_view(), name="search"),
-    path('items/<int:pk>', views.UserItemListView.as_view(), name="items"),
-    path('item/<int:pk>', views.ItemDetailView.as_view(), name="item"),
     path('user/<int:pk>', views.UserDetailView.as_view(), name="user"),
+    path('user/<int:pk>/items', views.UserItemListView.as_view(), name="items"),
+    path('user/<int:pk>/bids', views.UserBidListView.as_view(), name="bids"),
+    path('item/add', views.ItemCreateView.as_view(), name="add_item"),
+    path('item/<int:pk>', views.ItemDetailView.as_view(), name="item"),
+    path('item/<int:pk>/delete', views.ItemDeleteView.as_view(), name="delete_item"),
     path('bid/<int:pk>', views.BidView.as_view(), name="bid")
 ]
--- bidding/views.py
+++ bidding/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views import generic
 from django.http import JsonResponse, HttpResponse, QueryDict
 from django.core.exceptions import ValidationError
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
 from .models import User, Item, Bid
-from .forms import RegistrationForm
+from .forms import RegistrationForm, ItemCreateForm
+
+# Mixins
+class CurrentUserMixin(LoginRequiredMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated or kwargs.get('pk', None) != request.user.pk:
+            self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -25,16 +33,41 @@ class RegisterView(generic.CreateView):
         return super(RegisterView, self).form_valid(form)
 
 class UserItemListView(generic.ListView):
-    pass
+    model = User
+    template_name = "bidding/user_item_history.html"
+    context_object_name = 'items'
+
+    def get_queryset(self, **kwargs):
+        u = self.model.objects.get(pk=self.kwargs.get('pk'))
+        return u.item_set.all()
+
+class UserBidListView(CurrentUserMixin, generic.ListView):
+    model = User
+    permission_denied_message = "You can't view other people's bid history!"
+    template_name = "bidding/user_bid_history.html"
+    context_object_name = "bids"
+
+    def get_queryset(self, **kwargs):
+        u = self.model.objects.get(pk=self.kwargs.get('pk'))
+        return u.bid_set.all()
 
 class SearchListView(generic.ListView):
     pass
 
+class ItemCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Item
+    form_class = ItemCreateForm
+    template_name = "bidding/add_item.html"
+
+
 class ItemDetailView(generic.DetailView):
     pass
 
-class BidView(generic.TemplateView):
+class ItemDeleteView(generic.DeleteView):
     pass
+
+class BidView(LoginRequiredMixin, generic.TemplateView):
+    login_url = "/login"
 
 class UserDetailView(generic.DetailView):
     pass
