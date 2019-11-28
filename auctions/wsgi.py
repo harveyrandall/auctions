ad8da38c915fe3a4bd5b20e2c82801a432bdf176
Harvey Randall <harvey_randall@me.com>
2019-11-28 16:34:59 +0000

    gunicorn & wsgi confi update

--- auctions/wsgi.py
+++ auctions/wsgi.py
@@ -12,6 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auctions.settings')
-os.environ.setdefault('APP_CONFIG', 'conf.gunicorn_conf')
 
 application = get_wsgi_application()
