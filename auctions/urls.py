825d24a872e5c0337c988270abfe5b9f25af70d8
Harvey Randall <harvey_randall@me.com>
2019-11-28 13:28:17 +0000

    update urls to serve media when debug=False -- ref:https://ruddra.com/posts/deploy-django-to-openshift-3/

--- auctions/urls.py
+++ auctions/urls.py
@@ -14,13 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from bidding.views import health
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bidding.urls')),
-    path('health', health)
+    path('health', health),
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT
+    })
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
