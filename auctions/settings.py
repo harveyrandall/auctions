322228a540b17d71b3b5e2e894da7903362c911e
Harvey Randall <harvey_randall@me.com>
2021-03-10 01:50:54 +0000

    Set random secret key; TODO: store secret key in environment file not tracked by git and lload on each request so sessions & cookies aren't inavalidated when app is restarted

--- auctions/settings.py
+++ auctions/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+from django.core.management.utils import get_random_secret_key
 import os
 from . import database
 
@@ -21,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
