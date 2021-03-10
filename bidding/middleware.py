c3982f3404ce96134cf0f893ea898d36864061c6
Harvey Randall <harvey_randall@me.com>
2021-03-10 01:40:45 +0000

    Set document language in HTML tag and Content-Language header

---
+++ bidding/middleware.py
@@ -0,0 +1,8 @@
+class ContentLanguageMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response['Content-Language'] = "en"
+        return response
